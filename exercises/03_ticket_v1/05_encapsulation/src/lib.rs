c759644d8d90ed9c43b30613d0bb37f6cab0f203
Vadym S. Khondar <vadym@khondar.name>
2026-06-04 01:02:44 +0100

    Add solution for 03_05 encapsulation.

--- exercises/03_ticket_v1/05_encapsulation/src/lib.rs
+++ exercises/03_ticket_v1/05_encapsulation/src/lib.rs
@@ -29,11 +29,18 @@ pub mod ticket {
                 status,
             }
         }
-
-        // TODO: Add three public methods to the `Ticket` struct:
-        //  - `title` that returns the `title` field.
-        //  - `description` that returns the `description` field.
-        //  - `status` that returns the `status` field.
+        
+        pub fn title(&self) -> &str {
+            self.title.as_str()
+        }
+        
+        pub fn description(&self) -> &str {
+            self.description.as_str()
+        }
+        
+        pub fn status(&self) -> &str {
+            self.status.as_str()
+        }
     }
 }
 
